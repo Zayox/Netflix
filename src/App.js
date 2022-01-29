18a5620787d713ab56d3d54718e66a73ebc4f76b
Zayox <amirmoumen06@yahoo.fr>
2022-01-29 18:07:02 +0100

    everything is set up --> ready to go

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Row from "./components/Row";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <p>Hello wor ld</p>
+        <Row title="NETFLIX ORIGINALS" />
+        <Row title="Trending Now" />
     </div>
   );
 }
