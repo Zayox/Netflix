5b9cf1a941953bb164f704d532059cf40390db69
amirmoumen <amirmoumen06@yahoo.fr>
2022-02-02 06:54:22 -0800

    All data fetched / Rows styled (no scrollbar)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
 import React from 'react';
 import Row from "./components/Row";
+import requests from "./requests";
 
 function App() {
   return (
-    <div className="App">
-        <p>Hello wor ld</p>
-        <Row title="NETFLIX ORIGINALS" />
-        <Row title="Trending Now" />
+    <div>
+        <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals} isLargeRow/>
+        <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
+        <Row title="Top Rated" fetchURL={requests.fetchTopRated}/>
+        <Row title="Action Movies" fetchURL={requests.fetchActionMovies}/>
+        <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies}/>
+        <Row title="Horror Movies" fetchURL={requests.fetchHorrorMovies}/>
+        <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies}/>
+        <Row title="Documentaries" fetchURL={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/components/Row.jsx
+++ src/components/Row.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from '../axios';
 
-const Row = ({title}) => {
+const Row = ({fetchURL,title, isLargeRow}) => {
+
+    const base_url = "http://image.tmdb.org/t/p//original/";
+    const [movie, setMovie] = useState([]);
+
+    useEffect(()=>{
+
+        async function fetchData(){
+            let request = await axios.get(fetchURL);
+            setMovie(request.data.results);
+            console.log(request.data.results);
+        }
+        fetchData();
+
+    },[fetchURL])
+
+
+
     return(
       <div>
-            <h2>{title}</h2>
+          <h2 className="font-bold">{title}</h2>
+          <div className="flex overflow-y-hidden overflow-x-scroll p-8" id="row_img">
+              {movie.map(movie => <img src={base_url+`${isLargeRow ? movie.poster_path : movie.backdrop_path}`} alt={movie.name} className={`${isLargeRow ? "ml-5 border rounded-md duration-500 h-72 hover:scale-[1.1]" : "ml-5 border rounded-md duration-500 h-36 hover:scale-[1.1]"}`} key={movie.id}/>)}
+          </div>
       </div>
     );
 }
